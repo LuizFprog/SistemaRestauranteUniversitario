1ccbc48aa49c604dd7b8cc20c5ebd89d06c99aae
LuizFilipe <79083817+LuizFprog@users.noreply.github.com>
2023-03-11 17:57:21 -0300

    Revert "Modificando Repositorio e Interface"

--- src/br/com/ru/dados/IRepositorioGenerico.java
+++ src/br/com/ru/dados/IRepositorioGenerico.java
@@ -1,16 +1,5 @@
 package br.com.ru.dados;
 
-import java.util.List;
+public interface IRepositorioGenerico {
 
-public interface IRepositorioGenerico<T> {
-	
-	//public List<T> add(T conteudo) throws ElementoJaExisteException;
-	
-	public List<T> read();
-	
-	public void remove(T conteudo); //throws ElementoNaoExisteException;
-	
-	public void update(T novoConteudo); //throws ElementoNaoExisteException;
-
-	public void add(T novo); //throwsElementoJaExisteException;
 }
