cde8b16107a25be2f89e97a64e5f2ca8a9f57ca0
Raphael Melo <rm80838@gmail.com>
2023-03-11 16:48:36 -0300

    Modificando Classes de excessão

--- src/br/com/ru/exceptions/SaldoInsuficienteException.java
+++ src/br/com/ru/exceptions/SaldoInsuficienteException.java
@@ -1,5 +1,18 @@
 package br.com.ru.exceptions;
 
 public class SaldoInsuficienteException {
+    public int saldo;
 
+    public SaldoInsuficienteException(int saldo) {
+        super("O Saldo é insuficiente para a compra!");
+        this.saldo = saldo;
+    }
+
+    public int getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(int saldo) {
+        this.saldo = saldo;
+    }
 }
