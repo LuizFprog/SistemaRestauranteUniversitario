51c32a01232abba3fb4388df305612e65e5c7f62
LuizFprog <luizgomes.prog@gmail.com>
2023-03-11 17:36:07 -0300

    Modificando Repositorio e Interface

--- src/br/com/ru/exceptions/ElementoJaExisteException.java
+++ src/br/com/ru/exceptions/ElementoJaExisteException.java
@@ -1,5 +1,22 @@
 package br.com.ru.exceptions;
 
-public class ElementoJaExisteException {
+public class ElementoJaExisteException extends Exception{
 
+	private Object elemento;
+	
+	public ElementoJaExisteException(Object elemento)
+	{
+		super("Já existe");
+		this.elemento = elemento;
+	}
+
+	public Object getElemento() {
+		return elemento;
+	}
+
+	public void setElemento(Object elemento) {
+		this.elemento = elemento;
+	}
+	
+	
 }
