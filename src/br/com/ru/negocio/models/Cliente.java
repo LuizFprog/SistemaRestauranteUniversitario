360b6f8873ddc4185c3be19b603550eac422bdb0
Fontesx11 <fontesdgfranca@gmail.com>
2023-03-12 20:49:58 -0300

    Adicionando mudança no package models

--- src/br/com/ru/negocio/models/Cliente.java
+++ src/br/com/ru/negocio/models/Cliente.java
@@ -1,5 +1,37 @@
 package br.com.ru.negocio.models;
 
-public class Cliente {
+public class Cliente extends Usuario{
+	
+	
+	private int numeroFichas;
+	private double saldo;
+	
+	public Cliente(String primeiroNome, String ultimoNome, String cpf, String login, String senha) {
+		super(primeiroNome, ultimoNome, cpf, login, senha);
+		
+	}
+	
+	
+	public int getNumeroFichas() {
+		return numeroFichas;
+	}
 
+
+	public void setNumeroFichas(int numeroFichas) {
+		this.numeroFichas = numeroFichas;
+	}
+
+
+	public double depositar(double valor) {
+		return this.saldo = this.saldo + valor;
+	}
+	
+	public double getSaldo() {
+		return this.saldo;
+	}
+
+	@Override
+	public String toString() {
+		return "Cliente [numeroFichas=" + numeroFichas + ", saldo=" + saldo + "]";
+	}
 }
--- src/br/com/ru/negocio/models/Ficha.java
+++ src/br/com/ru/negocio/models/Ficha.java
@@ -1,5 +1,61 @@
 package br.com.ru.negocio.models;
 
 public class Ficha {
+	
+	private double precoAlmoco;
+	private double precoJanta;
+	private Cliente cliente;
+	private String codigo;
+	
+	public Ficha(double pAlmoco, double pJanta, Cliente cliente, String cod) {
+		
+		this.setPrecoAlmoco(pAlmoco);
+		this.setPrecoJanta(pJanta);
+		this.setCliente(cliente);
+		this.setCodigo(cod);
+		
+		
+	}
+	
+	public double getPrecoAlmoco() {
+		return precoAlmoco;
+	}
+	
+	public void setPrecoAlmoco(double precoAlmoco) {
+		this.precoAlmoco = precoAlmoco;
+	}
+	
+	public double getPrecoJanta() {
+		return precoJanta;
+	}
+	
+	public void setPrecoJanta(double precoJanta) {
+		this.precoJanta = precoJanta;
+	}
+	
+	public Cliente getCliente() {
+		return cliente;
+	}
+	
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+	
+	public String getCodigo() {
+		return codigo;
+	}
+	
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
 
+	@Override
+	public String toString() {
+		return "Ficha [precoAlmoco=" + precoAlmoco + ", precoJanta=" + precoJanta + ", cliente=" + cliente + ", codigo="
+				+ codigo + "]";
+	}
+	
+	
+	
+	
 }
