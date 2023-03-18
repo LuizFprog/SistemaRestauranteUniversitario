eddb62c96945182b0b9a2358f51d3927642fcc38
Fontesx11 <fontesdgfranca@gmail.com>
2023-03-18 12:22:47 -0300

    corrigindo detalhes

--- src/br/com/ru/negocio/models/Funcionario.java
+++ src/br/com/ru/negocio/models/Funcionario.java
@@ -4,8 +4,9 @@ public class Funcionario extends Usuario{
 
 		private String id;
 		
-		public Funcionario(String primeiroNome, String ultimoNome, String cpf, String login, String senha) {
+		public Funcionario(String primeiroNome, String ultimoNome, String cpf, String login, String senha, String id) {
 			super(primeiroNome, ultimoNome, cpf, login, senha);
+			this.id = id;
 			
 		}
 		
