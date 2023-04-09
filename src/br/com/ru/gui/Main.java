fe6dc677beb6e245371d49b65aecefe7065b96c6
Fontesx11 <fontesdgfranca@gmail.com>
2023-04-09 20:47:50 -0300

    Funcionou

--- src/br/com/ru/gui/Main.java
+++ src/br/com/ru/gui/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application{
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("TelaCadastroFuncionario.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("TelaQueFunciona.fxml"));
         Scene scene = new Scene(root, 800, 480);
         primaryStage.setScene(scene);
         primaryStage.show();
