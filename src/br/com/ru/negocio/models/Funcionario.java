92a96add91cac7e360cda4aa65b5d9845d002767
Fontesx11 <fontesdgfranca@gmail.com>
2023-04-17 12:45:18 -0300

    revisando funcionario

--- src/br/com/ru/negocio/models/Funcionario.java
+++ src/br/com/ru/negocio/models/Funcionario.java
@@ -1,25 +1,25 @@
 package br.com.ru.negocio.models;
 
-public class Funcionario extends Usuario{
+public class Funcionario extends Usuario {
 
-		private String id;
-		
-		public Funcionario(String primeiroNome, String ultimoNome, String cpf, String login, String senha, String id) {
-			super(primeiroNome, ultimoNome, cpf, login, senha);
-			this.id = id;			
-		}
+	private String id;
 
-		public String getId() {
-			return id;
-		}
+	public Funcionario(String primeiroNome, String ultimoNome, String cpf, String login, String senha, String id) {
+		super(primeiroNome, ultimoNome, cpf, login, senha);
+		this.id = id;
+	}
 
-		public void setId(String id) {
-			this.id = id;
-		}
-		
-		@Override
-		public String toString() {
-			return super.toString() + "Funcionario [id=" + id + "]";
-		}
-		
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + "Funcionario [id=" + id + "]";
+	}
+
 }
