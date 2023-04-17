01a28194734acece801671667375d0253104071d
Fontesx11 <fontesdgfranca@gmail.com>
2023-04-17 11:37:13 -0300

    revisao ControladorUsuario

--- src/br/com/ru/negocio/ControladorUsuario.java
+++ src/br/com/ru/negocio/ControladorUsuario.java
@@ -16,90 +16,87 @@ public class ControladorUsuario {
 	protected IRepositorioGenerico<Usuario> repositorioUsuario;
 	private static ControladorUsuario instancia;
 	private List<Usuario> listaUsuario;
-	
 
 	private ControladorUsuario() {
 		this.repositorioUsuario = new RepositorioUsuario(listaUsuario);
 	}
-	
+
 	public static ControladorUsuario getInstancia() {
 		if (instancia == null) {
-            instancia = new ControladorUsuario();
-        }
-        return instancia;
+			instancia = new ControladorUsuario();
+		}
+		return instancia;
 	}
-	
-	//Método para criar um novo cliente
-	public void criarCliente(String primeiroNome, String ultimoNome, String cpf, String login, String senha) throws ElementoJaExisteException {
-		
+
+	// Método para criar um novo cliente
+	public void criarCliente(String primeiroNome, String ultimoNome, String cpf, String login, String senha)
+			throws ElementoJaExisteException {
+
 		List<Usuario> usuarios = repositorioUsuario.ler();
-		
+
 		for (Usuario c : usuarios) {
 			if (c.getCpf().equals(cpf) || c.getLogin().equals(login)) {
 				throw new ElementoJaExisteException("Já existe um cliente com o mesmo CPF ou login!");
 			}
 		}
-		
+
 		if (primeiroNome != null && ultimoNome != null && cpf != null && login != null && senha != null) {
-			
-			
+
 			Cliente novo_cliente = new Cliente(primeiroNome, ultimoNome, cpf, login, senha, 0);
 			repositorioUsuario.inserir(novo_cliente);
-		}	
-		
-	
+		}
+
 	}
-	
+
 	public String listarTrabalhadorEspecifico(String cpf) throws ElementoNaoExisteException {
-		
+
 		Usuario c = recuperarUsuario(cpf);
-		
-		if(c instanceof Funcionario) {
+
+		if (c instanceof Funcionario) {
 			String funcionarioListado = c.toString();
 			return funcionarioListado;
 		}
-		
+
 		return null;
 	}
-	
-	public List<Cliente> listarTodosClientes(){
-		
+
+	public List<Cliente> listarTodosClientes() {
+
 		List<Usuario> atual = repositorioUsuario.ler();
 		List<Cliente> nova = new ArrayList<>();
-		
-		for(Usuario c : atual) {
-			
-			if(c instanceof Cliente) {
+
+		for (Usuario c : atual) {
+
+			if (c instanceof Cliente) {
 				nova.add((Cliente) c);
 			}
 		}
-	
+
 		return nova;
 	}
-	
-	public List<Funcionario> listarTodosFuncionarios(){
-		
+
+	public List<Funcionario> listarTodosFuncionarios() {
+
 		List<Usuario> atual = repositorioUsuario.ler();
 		List<Funcionario> nova = new ArrayList<>();
-		
-		for(Usuario f : atual) {
-			
-			if(f instanceof Funcionario) {
+
+		for (Usuario f : atual) {
+
+			if (f instanceof Funcionario) {
 				nova.add((Funcionario) f);
 			}
 		}
-	
+
 		return nova;
 	}
-	
+
 	public String listarClienteEspecifico(String cpf) throws ElementoNaoExisteException {
-		
+
 		Usuario c = recuperarUsuario(cpf);
-		
+
 		String clienteListado = c.toString();
 		return clienteListado;
-		}
-
+	}
 
 	// Método para recuperar um cliente
 	public Usuario recuperarUsuario(String cpf) throws ElementoNaoExisteException {
@@ -113,43 +110,40 @@ public class ControladorUsuario {
 		// Caso não encontre, lança exceção
 		throw new ElementoNaoExisteException("Não existe um cliente com esse CPF!");
 	}
-	
-	// Método para recuperar um cliente por login e senha
-		public Usuario recuperarLoginSenha(String login, String senha) throws ElementoNaoExisteException {
-			// Busca o cliente pelo CPF
-			List<Usuario> clientes = repositorioUsuario.ler();
-			for (Usuario c : clientes) {
-				if (c.getLogin().equals(login) && c.getSenha().equals(senha)) {
-					return c;
-				}
-			}
-			// Caso não encontre, lança exceção
-			throw new ElementoNaoExisteException("Não existe um cliente com esse CPF!");
-		}
 
-	
-	// Método para atualizar um cliente
-	public void atualizarCliente(String cpfAtual ,String primeiroNome, String ultimoNome,String login, String senha) throws ElementoNaoExisteException
-								{
-		
-		Cliente clienteAtual = (Cliente) recuperarUsuario(cpfAtual);
-		
-		Cliente novo = new Cliente(primeiroNome, ultimoNome, clienteAtual.getCpf(), login, senha, clienteAtual.getSaldo());
-		repositorioUsuario.atualizar(clienteAtual, novo);	
+	// Método para recuperar um cliente por login e senha
+	public Usuario recuperarLoginSenha(String login, String senha) throws ElementoNaoExisteException {
+		// Busca o cliente pelo CPF
+		List<Usuario> clientes = repositorioUsuario.ler();
+		for (Usuario c : clientes) {
+			if (c.getLogin().equals(login) && c.getSenha().equals(senha)) {
+				return c;
+			}
+		}
+		// Caso não encontre, lança exceção
+		throw new ElementoNaoExisteException("Não existe um cliente com esse CPF!");
 	}
-	
-	public void depositarDinheiro(double valor, String cpf) throws ElementoNaoExisteException
-	{
+
+	// Método para atualizar um cliente
+	public void atualizarCliente(String cpfAtual, String primeiroNome, String ultimoNome, String login, String senha)
+			throws ElementoNaoExisteException {
+
+		Cliente clienteAtual = (Cliente) recuperarUsuario(cpfAtual);
+
+		Cliente novo = new Cliente(primeiroNome, ultimoNome, clienteAtual.getCpf(), login, senha,
+				clienteAtual.getSaldo());
+		repositorioUsuario.atualizar(clienteAtual, novo);
+	}
+
+	public void depositarDinheiro(double valor, String cpf) throws ElementoNaoExisteException {
 		Cliente clienteAtual = (Cliente) recuperarUsuario(cpf);
 		clienteAtual.depositar(valor);
 	}
-	
-	public void debitarDinheiro(double valor, String cpf) throws ElementoNaoExisteException
-	{
+
+	public void debitarDinheiro(double valor, String cpf) throws ElementoNaoExisteException {
 		Cliente clienteAtual = (Cliente) recuperarUsuario(cpf);
 		clienteAtual.debitar(valor);
 	}
-	
 
 	// Método para excluir um cliente
 	public void excluirCliente(String cpf) throws ElementoNaoExisteException {
@@ -158,34 +152,34 @@ public class ControladorUsuario {
 		// Remove o cliente do repositório
 		repositorioUsuario.remover(cliente);
 	}
-	
+
 	// Metodo para adicionar Funcionario
-	public void adicionarFuncionario(String primeiroNome, String ultimoNome, String cpf, String login, String senha, String id) throws ElementoJaExisteException {
-		
+	public void adicionarFuncionario(String primeiroNome, String ultimoNome, String cpf, String login, String senha,
+			String id) throws ElementoJaExisteException {
+
 		List<Usuario> usuarios = repositorioUsuario.ler();
-		
+
 		for (Usuario c : usuarios) {
-			if (c.getCpf().equals(cpf) || c.getLogin().equals(login)){
+			if (c.getCpf().equals(cpf) || c.getLogin().equals(login)) {
 				throw new ElementoJaExisteException("Já existe um usuario com o mesmo CPF ou login!");
 			}
-			if(c instanceof Funcionario && ((Funcionario)c).getId().equals(id))
-			{
+			if (c instanceof Funcionario && ((Funcionario) c).getId().equals(id)) {
 				throw new ElementoJaExisteException("Já existe um funcionario com o mesmo id");
 			}
 		}
-		
+
 		if (primeiroNome != null && ultimoNome != null && cpf != null && login != null && senha != null && id != null) {
 			Funcionario novo = new Funcionario(primeiroNome, ultimoNome, cpf, login, senha, id);
 			repositorioUsuario.inserir(novo);
 		}
-		
+
 	}
-	
+
 	public List<Usuario> listarUsuarios() {
-	
-        return repositorioUsuario.ler();
-    }
-	
+
+		return repositorioUsuario.ler();
+	}
+
 	// Recuperar funcionario
 	public Usuario recuperarFuncionario(String cpf) throws ElementoNaoExisteException {
 		// Busca o cliente pelo CPF
@@ -198,22 +192,22 @@ public class ControladorUsuario {
 		// Caso não encontre, lança exceção
 		throw new ElementoNaoExisteException("Não existe um cliente com esse CPF!");
 	}
-	
+
 	public void excluirFuncionario(String cpf) throws ElementoNaoExisteException {
 		// Busca o cliente pelo CPF
 		Funcionario funcionario = (Funcionario) recuperarUsuario(cpf);
-		// Remove o cliente do repositório	
+		// Remove o cliente do repositório
 		repositorioUsuario.remover(funcionario);
-		
+
 	}
-	
-	public void atualizarFuncionario (String cpfAtual, String primeiroNome, String ultimoNome, String login, String senha, String id) 
-					throws ElementoNaoExisteException {
-		
+
+	public void atualizarFuncionario(String cpfAtual, String primeiroNome, String ultimoNome, String login,
+			String senha, String id) throws ElementoNaoExisteException {
+
 		Funcionario funcionarioAtual = (Funcionario) recuperarUsuario(cpfAtual);
-		
+
 		Funcionario novo = new Funcionario(primeiroNome, ultimoNome, funcionarioAtual.getCpf(), login, senha, id);
 		repositorioUsuario.atualizar(funcionarioAtual, novo);
-		
+
 	}
 }
